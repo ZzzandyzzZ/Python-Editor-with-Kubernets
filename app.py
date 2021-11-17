905b4e4ba14202b9cc31a52f83eb0c02aa8cf250
Andy <andy@apurata.com>
2021-11-17 10:24:07 -0500

    Add execute code

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
+from io import StringIO
+import sys
+import json
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['GET', 'POST'])
 def ping():
     return  "PYTHON EXECUTOR WORKING FINE"
 
 
 @app.route('/execute',methods=['POST'])
 def executePython():
-    data = request.json
-    code = data['code']
-    print(code)
+    #print('This is error output', file=sys.stderr)
 
-    return jsonify(code)
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    error= ""
+    try:
+        exec(json.loads(request.data))
+    except Exception as e:
+        sys.stdout = old_stdout
+        error += ("\n ERRORS: \n" + repr(e))
+    output = redirected_output.getvalue()
+    output += error
+    return output
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
