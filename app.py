baa039f854ac51e11c7eb40b30a7ff555a518465
Andy <andy@apurata.com>
2021-11-17 06:57:41 -0500

    Add CORS

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/',methods=['GET'])
 def ping():
-    return  "FUNCA BIEN"
+    return  "PYTHON EXECUTOR WORKING FINE"
 
+
+@app.route('/execute',methods=['POST'])
+def executePython():
+    data = request.json
+    code = data['code']
+    print(code)
+
+    return jsonify(code)
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
